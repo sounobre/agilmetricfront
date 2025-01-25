03c226a52096903ceef421b896d94209dddbe3a8
Diego Nobre Correa <dcorrea@brasilseg.com.br>
2025-01-25 18:47:04 -0300

    a

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://agilmetricback-production.up.railway.app/api',
 });
 
 export default api;
